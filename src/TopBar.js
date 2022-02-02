ab93634ad1c6e836dda4659908c2fe2309d19e38
unknown <Amit1paz@gmail.com>
2022-02-02 10:21:43 +0200

    style

--- src/TopBar.js
+++ src/TopBar.js
@@ -4,10 +4,13 @@ import Notifications from './imgs/Notifications.svg'
 const Topbar = ({ activeTab }) => {
     return (
         <div className='top-bar'>
-            <h2>{activeTab}</h2>
+            <h2 className='top-bar__headline'>{activeTab}</h2>
             <div className='top-bar__left-side'>
-                <img className='notifications' src={Notifications} alt='Notifications'/>
-                <img className='user-image' src='https://avatars.githubusercontent.com/u/64500653?v=4' alt='User'/>
+                <div className='notifications-wrap'>
+                    <img className='notifications' src={Notifications} alt='Notifications'/>
+                    <div className='notifications-circle'></div>
+                </div>
+                <img className='user-img' src='https://avatars.githubusercontent.com/u/64500653?v=4' alt='User'/>
             </div>
         </div>
     );
