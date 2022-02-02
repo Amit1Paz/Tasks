f53030a99b653ef59d6dae5e320cd700ae95429a
unknown <Amit1paz@gmail.com>
2022-02-02 15:13:36 +0200

    sort list style

--- src/Home/TaskList.js
+++ src/Home/TaskList.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Tasklist = () => {
-    const SortList = ['Name', 'Date', 'Status', 'Custom'];
+    const [sortList, setSortList] = useState([
+        {name: 'Name', selected: false, className: null},
+        {name: 'Date', selected: false, className: null},
+        {name: 'Status', selected: false, className: null},
+        {name: 'Custom', selected: false , className: null}
+    ]);
 
+    const [selectedSort, setSelectedSort] = useState(sortList[0].name);
+
+    const handleSortClick = (e) => {
+        setSelectedSort(e.target.innerText)
+    }
+    useEffect(() => {
+        if (selectedSort) {
+            console.log(selectedSort)
+            const newSortList = [...sortList];
+            newSortList.map(item => item.className = null);
+            const sort = newSortList.find(item => item.name === selectedSort);
+            sort.className = 'selected-sort';
+            setSortList(newSortList)
+        }
+    }, [selectedSort])
+    
     return (
         <div className='tasks-list-container'>
             <h3>Tasks:</h3>
             <div className='sort'>
                 <h5>Sort:</h5>
                 <ul>
-                    {SortList.map(item => {
-                        return <li key={item}>{item}</li>
+                    {sortList.map(item => {
+                        return <li key={item.name} onClick={handleSortClick} className={item.className}>{item.name}</li>
                     })}
                 </ul>
             </div>
