c6f7268857fc8c63b102c3dcae68c95d85e7265d
unknown <Amit1paz@gmail.com>
2022-02-02 11:30:13 +0200

    form structure

---
+++ src/Home/AddTaskForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Addtaskform = () => {
+    return (
+        <form className='add-task-form'>
+            <label for='task-input'>Task</label>
+            <input id='task-input' type='text' placeholder='Insert task here...'/>
+            <label for='task-schedule-date'>Schedule date</label>
+            <input id='task-schedule-date' type='date'/>
+            <label for='task-schedule-time'>Schdule time</label>
+            <input id='task-schedule-time' type='time'/>
+        </form>
+    );
+}
+
+export default Addtaskform;
--- src/Home/HomeHeader.js
+++ src/Home/HomeHeader.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import '../css/home.css'
+import AddTaskForm from './AddTaskForm';
 const Homeheader = ({ name }) => {
     const [showAddForm, setShowAddForm] = useState(false);
     const handleAddTaskClick = () => {
         setShowAddForm(!showAddForm)
     }
-    console.log(showAddForm)
+
     return (
         <div className='home-header-container'>
             <div className='greeting'>
                 <h3>Hi {name},</h3>
                 <p>Let's be productive</p>
             </div>
+            { showAddForm && <AddTaskForm /> }
             <button onClick={handleAddTaskClick} className='add-task-btn'>+</button>
         </div>
     );
