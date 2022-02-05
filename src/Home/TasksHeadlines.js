e17b13aca8c187772d2441420ad7884a54d4e8c1
unknown <Amit1paz@gmail.com>
2022-02-05 21:12:50 +0200

    headlines

---
+++ src/Home/TasksHeadlines.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TasksHeadlines = () => {
+    return (
+        <ul className='task-headlines'>
+            <li>Task</li>
+            <li>Priority</li>
+            <li>Status</li>
+            <li>Date</li>
+            <li></li>
+        </ul>
+    );
+}
+
+export default TasksHeadlines;
