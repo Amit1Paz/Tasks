720ea13ab8a4e4284019c1dd4ca2e47ce151e11f
unknown <Amit1paz@gmail.com>
2022-02-05 19:04:58 +0200

    spaces

--- src/Home/AddTaskForm.js
+++ src/Home/AddTaskForm.js
@@ -2,7 +2,7 @@ import React, { useRef, useContext } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import TasksListContext from '../Contexts/TasksListContext';
 
-const Addtaskform = ({ setShowAddForm}) => {
+const Addtaskform = ({ setShowAddForm }) => {
     const [tasksList, setTasksList] = useContext(TasksListContext);
     const taskRef = useRef();
     const dateRef = useRef();
