a0feef84c4b4176696062d9907e4243009ee5ae4
unknown <Amit1paz@gmail.com>
2022-02-09 21:28:10 +0200

    need to sort dates with reduce

--- src/Achievements/WeeklyChart.js
+++ src/Achievements/WeeklyChart.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS } from 'chart.js/auto'
+import DoneListContext from '../Contexts/DoneListContext';
 
 const WeeklyChart = () => {
+    const [doneList, setDoneList] = useContext(DoneListContext);
+
     const labels = [];
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 7; i++) {
         const d = new Date();
         d.setDate(d.getDate()-i);
         const day = `${d.getDate()}.${d.getMonth() + 1}`
         labels.unshift(day)
     }
+
+    
+
+    useEffect(() => {
+        const sort = [...doneList]
+        sort.map(task => {
+            const d = new Date(task.dateInMs);
+            console.log(d.getDay())
+        })
+    }, [doneList])
 
     return (
         <div className='weekly-chart-container'>
@@ -20,10 +33,11 @@ const WeeklyChart = () => {
             data={{
                 labels: labels,
                 datasets: [{
-                    data: [12, 19, 0, 5, 2, 3],
+                    data: [],
                     backgroundColor: 'rgb(99, 99, 99)',
                     borderColor: 'rgb(204, 204, 204)',
-                    borderWidth: 1,  
+                    borderWidth: 1,
+                    maxBarThickness: 20
                 }],
             }}
             options={{
